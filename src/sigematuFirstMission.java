5e1d5474c724a30d4b94930355399898ccd651db
unknown <>
2016-08-29 11:44:56 +0900

    1

---
+++ src/sigematuFirstMission.java
@@ -0,0 +1,17 @@
+package mymission;
+public class sigematuFirstMission{
+// インスタンス変数
+private String name;
+// コンストラクタ
+public sigematuFirstMission(){
+    name = "sigematu";
+}
+// セッタ
+public void setName(String name){
+    this.name = name;
+}
+// ゲッタ
+public  String getName(){
+    return name;
+}
+}
