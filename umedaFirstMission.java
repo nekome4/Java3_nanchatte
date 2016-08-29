8bf646af145bb5d3e3d5ac7e9118765a95369308
umechan <2741003@bemax.jp>
2016-08-29 12:09:02 +0900

    梅田ふぁーすとみっしょんです (#1)

---
+++ umedaFirstMission.java
@@ -0,0 +1,17 @@
+package mymission;
+public class umedaFirstMission{
+// インスタンス変数
+private String name;
+// コンストラクタ
+public umedaFirstMission(){
+    name = "umeda";
+}
+// セッタ
+public void setName(String name){
+    this.name = name;
+}
+// ゲッタ
+public  String getName(){
+    return name;
+}
+}
