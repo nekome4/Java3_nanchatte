c0ac7875d86aa585405d29507333f2fff3c93e56
ikt3 <2741001@bemax.jp>
2016-08-29 12:41:34 +0900

    いもと

---
+++ imotoFirstMission.java
@@ -0,0 +1,17 @@
+package mymission;
+public class imotoFirstMission{
+// インスタンス変数
+private String name;
+// コンストラクタ
+public imotoFirstMission(){
+    name = "imoto";
+}
+// セッタ
+public void setName(String name){
+    this.name = name;
+}
+// ゲッタ
+public  String getName(){
+    return name;
+}
+}
